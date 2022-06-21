90ade4743d40c7bd6242ce193c83c3e7c30df64f
MariusJP <MJTheOne@gmail.com>
2022-06-21 11:39:11 +0200

    PSR-12

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -1,33 +1,20 @@
 <?php
 
-namespace Ampeco\Filters;
+declare(strict_types=1);
+
+namespace Coddin\Filters;
 
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
 
-class FieldServiceProvider extends ServiceProvider
+final class FieldServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-date-range-filter', __DIR__ . '/../dist/js/date-range-filter.js');
             Nova::style('nova-date-range-filter', __DIR__ . '/../dist/css/date-range-filter.css');
         });
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
